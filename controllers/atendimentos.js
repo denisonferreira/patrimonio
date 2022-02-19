e72a890ca64a79a77efffa562b2bffc076188f81
Denison Ferreira <denison.ferreira@hotmail.com.br>
2022-02-19 00:22:38 -0300

    subindo o projeto exemplo

---
+++ controllers/atendimentos.js
@@ -0,0 +1,16 @@
+const Atendimento = require('../models/atendimentos')
+
+module.exports = app => {
+    app.get('/', (req, res) => res.send('servidor rodando, tudo ok!'))
+    
+    app.get('/atendimentos', (req, res) => res.send('você está na rota de atendimentos e está realizando um GET.'))
+
+    app.post('/atendimentos', (req, res) => {
+        //console.log(req.body)
+        const atendimento = req.body
+
+        Atendimento.adiciona(atendimento)
+        res.send('você está na rota de atendimentos e está realizando um Post.')
+    })
+    
+}
