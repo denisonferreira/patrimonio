bd2342aa5616d2cb00993d1771352a9dea08b9c7
Denison Ferreira <denison.ferreira@hotmail.com.br>
2022-07-22 05:47:14 -0300

    add novos campos

--- patrim-app/controllers/itens.js
+++ patrim-app/controllers/itens.js
@@ -22,7 +22,7 @@ module.exports = app => {
     })
     app.get('/itens', (req, res) => {
         //   res.send('você está na rota de itens e está realizando um GET.'))
-        res.send("<!-- #######  Projeto Patrimonio #########--><h1 style='color: #5e9ca0;'>Sistema de Patrimônio</span></h1><h2 style='color: #2e6c80;'>System Idiomas</h2><form action='/patrimonio' method='post'><label for='fname'>PATRIMONIO:</label><input type='text' id='patrimonio' name='patrimonio'><br><br><label for='lname'>TIPO:</label><input type='text' id='tipo' name='tipo'><br><br><input type='submit' value='Submit'></form>")
+        res.send("<!-- #######  Projeto Patrimonio #########--><h1 style='color: #5e9ca0;'>Sistema de Patrimônio</span></h1><h2 style='color: #2e6c80;'>System Idiomas</h2><form action='/patrimonio' method='post'><label for='fname'>PATRIMONIO:</label><input type='text' id='patrimonio' name='patrimonio'><br><br><label for='lname'>TIPO:</label><input type='text' id='tipo' name='tipo'><br><br><label for='ldataentrada'>Data Entrada:</label><input type='text' id='dataentrada' name='dataentrada'><br><br><label for='ldatasaida'>Data Saida:</label><input type='text' id='datasaida' name='datasaida'><br><br><input type='submit' value='Submit'></form>")
 
     })
     app.post('/itens', (req, res) => {
