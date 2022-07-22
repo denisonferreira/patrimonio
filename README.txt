ce0981cb348ee0c24b8e5425850e9e6f5df7abbd
Denison Ferreira <denison.ferreira@hotmail.com.br>
2022-07-22 05:37:28 -0300

    Bug detectado ao criar tabela na primeira vez e contornado

--- README.txt
+++ README.txt
@@ -1,6 +1,9 @@
-rode o dockercomposer
-#docker-compose up --build -d
+// rode o dockercomposer
+// #docker-compose up --build -d
 
-e acesse localhost:6868
+// e acesse localhost:6868
 
-obs.: caso não funcione é porque o data/db tá subindo para o git e precisa ser incluído no gitignore
+// obs.: caso não funcione é porque o data/db tá subindo para o git e precisa ser incluído no gitignore
+
+//BUG quando ele inicia pela primeira vez não é criada a tabela do banco
+//Para resolver é só dar um #docker stop patrimonio_app_1 e depois um docker start patrimonio_app_1
--- patrim-app/infraestrutura/tabelas.js
+++ patrim-app/infraestrutura/tabelas.js
@@ -1,13 +1,11 @@
 class Tabelas {
     init(conexao) {
         this.conexao = conexao
-
         this.criarItens()
     }
 
     criarItens() {
-        const sql = 'CREATE TABLE IF NOT EXISTS Patrim (id int NOT NULL AUTO_INCREMENT, patrimonio varchar(50) NOT NULL, tipo varchar(20), dataentrada varchar(20), datasaida varchar(20), status varchar(20), observacoes text, PRIMARY KEY(id))'
-
+        const sql = 'CREATE TABLE IF NOT EXISTS Patrim (id int NOT NULL AUTO_INCREMENT, patrimonio varchar(50) NOT NULL, tipo varchar(20), dataentrada varchar(20), datasaida varchar(20), status varchar(20), observacoes text, PRIMARY KEY(id))';
         this.conexao.query(sql, erro => {
             if (erro) {
                 console.log(erro)
